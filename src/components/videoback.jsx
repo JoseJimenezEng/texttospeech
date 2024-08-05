8ad682ea2ba40f6bfd5b051aeb5f6604db7df5e7
JoseJimenezEng <jjmarts@gmail.com>
2024-08-04 20:56:40 -0500

    fixed bgvideo before footer

--- src/components/videoback.jsx
+++ src/components/videoback.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import videobgTts from '../assets/bgvidtts.mp4'
 
-const videoback = () => {
+
+const Videoback = () => {
+    const videoRefBg = useRef(null);
+
+    useEffect(() => {
+        const video = videoRefBg.current;
+        if (video) {
+            video.play();
+        }
+    }, []);
+
     return (
-        <div className="videobackground">
-                    <video src={videobgTts} autoPlay loop></video>
+       
+            <div className="videoBackground">
+                
+                <video
+                    ref={videoRefBg}
+                    src={videobgTts}
+                    autoPlay
+                    playsInline
+                    preload="metadata"
+                    muted
+                    loop
+                    id="myVideo"
+                    onError={(e) => console.error('Error playing video:', e)}
+                ></video>
+
             </div>
+        
     );
 };
 
-export default videoback;
+export default Videoback;
